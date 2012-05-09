1ffa7060be3b8c1f7fc6b6838a7c67a04cde2ae8
= <=>
2012-05-09 14:44:32 +0200

    delete experimental

--- experimental/commit.cmd
+++
@@ -1,8 +0,0 @@
-@echo ------------------------------------------------
-@echo 2. Let the commit-repo do the svn-stuff
-cd git_svn_commit_repo
-call git pull --rebase 
-call git svn rebase
-call git svn dcommit 
-
-cd..
--- experimental/dev_1_add.cmd
+++
@@ -1,9 +0,0 @@
-set FILE=file_%1%.html
-
-cd git_dev1
-call touch %FILE%
-call git add %FILE%
-call git commit -m"%FILE% commited from myscript"
-call git pull 
-call git push
-cd..
