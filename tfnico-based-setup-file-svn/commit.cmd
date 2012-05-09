c6279626875dd5e49380a2d2d2b3a51cb5791aaa
= <=>
2012-05-09 15:19:41 +0200

    removed old PoCs not cutting it

--- tfnico-based-setup-file-svn/commit.cmd
+++
@@ -1,8 +0,0 @@
-@echo ------------------------------------------------
-@echo 2. Let the commit-repo do the svn-stuff
-cd git_svn_commit
-call git pull --rebase
-call git svn reset 100000
-call git svn rebase
-call git svn dcommit 
-cd..
