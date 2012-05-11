3ee9adf2309d1d97103db633973a0b64f9a94af5
= <=>
2012-05-11 18:14:12 +0200

    more succeess

--- testit.groovy
+++ testit.groovy
@@ -1,3 +1,7 @@
+enum SCM {
+    svn, git
+}
+
 class ScmExecutable {
 	String username
 	
@@ -5,15 +9,12 @@ class ScmExecutable {
     String svnRepoDir;
 	def ant = new AntBuilder();
 	
-	def git(String command ) {
-		git(command, "");
-	}
-	
-	def git(String command,String argument) {		
-		println(username +" ->GIT: " + command + " " + argument );		
+	def git(String command,String ... arguments) {		
+		println(username +" ->GIT: " + command + " " + arguments );		
 		ant.exec(executable:"git",dir:gitRepoDir,resultproperty:"cmdExit") {
 			arg(value:command)
-			if( "" != argument ) { 
+		
+			for( argument in arguments ) { 
 				arg(value:argument)
 			}
 		}
@@ -31,11 +32,6 @@ class ScmExecutable {
 		if( "0" != "${ant.project.properties.cmdExit}" ) throw new RuntimeException("Error executing ant command: " + command );						
 	}	
 }
-
-enum SCM {
-    svn, git
-}
-
 
 class Dev extends ScmExecutable {
 	
@@ -136,23 +132,21 @@ class Dev extends ScmExecutable {
 		return this;
 	}
 	
-	def append( String content ) {
-		 
-		def fileToAppend = svnRepoDir + "/" + svn_branch + "/" + projectDir + "/" + currentFile
-		
-		println( "Adding <" + content + "> to ["+fileToAppend+"]");  
-				
-		ant.echo(file:fileToAppend,append:true,message:System.getProperty("line.separator")+content);
-		
+	def append( String content ) {		 
+		def fileToAppend = svnRepoDir + "/" + svn_branch + "/" + projectDir + "/" + currentFile		
+		println( "Adding <" + content + "> to ["+fileToAppend+"]");  				
+		ant.echo(file:fileToAppend,append:true,message:System.getProperty("line.separator")+content);		
 		return this;
 	}
 	
 	// jenkins
 	def pull( String option ) {
+		git("pull",option);
 		return this;
 	}
 	
 	def svn_reset( Integer revision ) {
+		git("svn","reset",revision.toString());
 		return this;
 	}
 
@@ -177,22 +171,22 @@ ola.svn().goto_branch( "kaksi").chdir("model/src/main/mod").on_file( "domain.mod
 ola.git().checkout( "kaksi").chdir("web/src/main/webapp").add( "ola-foo.html", "ola-foo-view.html" ).commit().push();
 ola.svn().commit();
 
-siv.svn().goto_branch( "yksi").chdir("model/src/main/mod").on_file( "domain.mod" ).append( "SivBar" );
-siv.git().checkout( "yksi").chdir("web/src/main/webapp").add( "siv-bar.html", "siv-bar-view.html" ).commit().push();
-siv.svn().commit();
-*/
 per.svn().goto_trunk().chdir("model/src/main/mod").on_file( "domain.mod" ).append( "PerBaz" );
 per.git().checkout( "trunk").chdir("web/src/main/webapp").add( "per-baz.html", "per-baz-view.html" ).commit().push();
 per.svn().commit();
 
+siv.svn().goto_branch( "yksi").chdir("model/src/main/mod").on_file( "domain.mod" ).append( "SivBar" );
+siv.git().checkout( "yksi").chdir("web/src/main/webapp").add( "siv-bar.html", "siv-bar-view.html" ).commit().push();
+siv.svn().commit();
+*/
 
-/*
+
 jenkins.git().checkout( "trunk").pull("--rebase").svn_reset(2147483647).svn_rebase().svn_dcommit();
 jenkins.git().checkout( "kaksi").pull("--rebase").svn_reset(2147483647).svn_rebase().svn_dcommit();
 jenkins.git().checkout( "yksi").pull("--rebase").svn_reset(2147483647).svn_rebase().svn_dcommit();
 
 println("VERIFY")
-*/
+
 
 /*
 ola.git().checkout( "kaksi" ).chdir( "web" ).touch_and_add( "readme.txt" ).commit().push();
