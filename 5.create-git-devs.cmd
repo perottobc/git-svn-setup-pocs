5a84e19a10f094642093bfc06c6d90853d13334d
perottobc <perottobc@gmail.com>
2012-05-13 15:33:06 +0200

    more script refactoring

--- 5.create-git-devs.cmd
+++ 5.create-git-devs.cmd
@@ -10,27 +10,27 @@ mkdir %WDIR%\devs\per
 mkdir %WDIR%\devs\siv
 mkdir %WDIR%\devs\ola
 
-call git clone -o mirror %WDIR%\devs\adm\websites.git %WDIR%\devs\per\git_websites
+call git clone %WDIR%\devs\adm\websites.git %WDIR%\devs\per\git_websites
 cd %WDIR%\devs\per\git_websites
 call git config user.name "per"
 call git config user.email "per@doit.com"
-call git checkout -t mirror/trunk
+call git checkout -t remotes/origin/svn/trunk
 call git pull --rebase
 
 
-call git clone -o mirror %WDIR%\devs\adm\websites.git %WDIR%\devs\ola\git_websites
+call git clone %WDIR%\devs\adm\websites.git %WDIR%\devs\ola\git_websites
 cd %WDIR%\devs\ola\git_websites
 call git config user.name "ola"
 call git config user.email "ola@doit.com"
-call git checkout -t mirror/kaksi
+call git checkout -t remotes/origin/svn/kaksi
 call git pull --rebase
 
 
-call git clone -o mirror %WDIR%\devs\adm\websites.git %WDIR%\devs\siv\git_websites
+call git clone %WDIR%\devs\adm\websites.git %WDIR%\devs\siv\git_websites
 cd %WDIR%\devs\siv\git_websites
 call git config user.name "siv"
 call git config user.email "siv@doit.com"
-call git checkout -t mirror/yksi
+call git checkout -t remotes/origin/svn/yksi
 call git pull --rebase
 
 
--- setup-all.cmd
+++ setup-all.cmd
@@ -1,8 +1,7 @@
-SET SCRIPT_DIR=e:\git-svn
 call %SCRIPT_DIR%\1.init-svn-repo-on-apache.cmd
 call %SCRIPT_DIR%\2.init-svn-repo-with.compay-repo-template.cmd
 call %SCRIPT_DIR%\3.create-branches.cmd
-call %SCRIPT_DIR%\4.1.create-gatekeeper-and-bare.cmd
-call %SCRIPT_DIR%\5.1.create-git-devs.cmd
+call %SCRIPT_DIR%\4.create-gatekeeper-and-bare.cmd
+call %SCRIPT_DIR%\5.create-git-devs.cmd
 call %SCRIPT_DIR%\6.create-svn-for-devs.cmd
 cd %SCRIPT_DIR%
