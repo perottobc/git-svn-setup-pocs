5a176b6fea1a3e6bcdd13fb20287a2525b90ec48
per <per@doit.com>
2012-05-13 12:25:40 +0200

    ajusted msgs

--- check_env_var.cmd
+++ check_env_var.cmd
@@ -1,26 +1,26 @@
 @echo off
 
 if "%WDIR%" == "" GOTO WDIR_EXIT
-@echo Working directory set to %WDIR%
+@echo Info: Working directory set to %WDIR%
 
 if '%EDITOR%' == '' GOTO EDITOR_EXIT
-@echo Editor set to %EDITOR%
+@echo Info: Editor set to %EDITOR%
 
 if '%HTPASSWD_EXE%' == '' GOTO HTPASSWD_EXE_EXIT
-@echo htpasswd set to %HTPASSWD_EXE%
+@echo Info: htpasswd set to %HTPASSWD_EXE%
 
 GOTO CONTINUE_SETUP
 
 :WDIR_EXIT
-@echo Define working directory where testing will be done, e.g.:  set WDIR=e:\
+@echo Error: Define working directory where testing will be done, e.g.:  set WDIR=e:\
 Exit /b
 
 :EDITOR_EXIT
-@echo Define an editor with the env-variable EDITOR, e.g set EDITOR=notepad++
+@echo Error: Set env-var EDITOR, e.g set EDITOR="C:\Program Files (x86)\Notepad++\notepad++.exe"
 Exit /b
 
 :HTPASSWD_EXE_EXIT
-@echo Define apache2 home, e.g. "C:\Programfiler\Apache Software Foundation\Apache2.2\bin\htpasswd"
+@echo Error: Set env-var HTPASSWD_EXE, e.g. set HTPASSWD_EXE="C:\Program Files (x86)\Apache Software Foundation\Apache2.2\bin\htpasswd"
 Exit /b
 
 :CONTINUE_SETUP
